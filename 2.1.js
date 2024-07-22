d955ad7120272014acddb9329bba34c93844cefb
irakli <iraklitsurtsumia@gmail.com>
2024-07-22 19:11:36 +0400

    1

---
+++ 2.1.js
@@ -0,0 +1,6 @@
+console.log("Name is : Ira");
+console.log("Surname is : Tsu");
+console.log("Age : 3911");
+console.warn("Email : Example@idunno.com");
+console.warn("Password : idunno");
+console.log("Phone : 555-555-555");
